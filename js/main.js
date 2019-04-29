24e955448443f110e6b9fee0e935e6471344feee
Brian Berk <brianjberk@gmail.com>
2019-04-29 13:28:09 -0400

    add job listings

--- js/main.js
+++ js/main.js
@@ -170,5 +170,33 @@ jQuery(document).ready(function ($) {
     }
   });
 
+  // read TSV file
+  $.ajax({
+    type: "GET",
+    url: "data/job_listings.tsv",
+    dataType: "text",
+    success: function(data) {
+      processData(data);
+    }
+  });
+
+  function processData(allText) {
+    // var allTextLines = allText.split(/\r\n|\n/);
+    // var headers = allTextLines[0].split(',');
+    // var lines = [];
+
+    // for (var i = 0; i < allTextLines.length; i++) {
+    //   var data = allTextLines[i].split(',');
+    //   if (data.length == headers.length) {
+
+    //     var tarr = [];
+    //     for (var j = 0; j < headers.length; j++) {
+    //       tarr.push(headers[j] + ":" + data[j]);
+    //     }
+    //     lines.push(tarr);
+    //   }
+    // }
+    console.log(allText);
+  }
 
 });
